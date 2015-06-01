64f59f35b7fa73c9e2d359baad4ba16b510cc74c
omnitool <biting_duck@hotmail.com>
2015-06-01 01:31:38 -0600

    adding first,last, username

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,11 @@ RSpec.describe User, type: :model do
   it 'saves all attributes' do
     user = User.new({first_name: 'Philadelphia', last_name: 'Collins', username: 'baaaam', email: 'phil@collins.info', password: 'peanutbutterandjaaaam'})
     user.save!
-    expect(user).to match({first_name: 'Philadelphia', last_name: 'Collins', username: 'baaaam', email: 'phil@collins.info', password: 'peanutbutterandjaaaam'})
+    # expect(user).to match({first_name: 'Philadelphia', last_name: 'Collins', username: 'baaaam', email: 'phil@collins.info', password: 'peanutbutterandjaaaam'})
+    expect(user).to be_valid
+    expect(user.first_name).to eq 'Philadelphia'
+    expect(user.last_name).to eq 'Collins'
+    expect(user.username).to eq 'baaaam'
+    expect(user.email).to eq 'phil@collins.info'
   end
 end
