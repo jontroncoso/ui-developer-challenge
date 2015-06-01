f082acfb29bcea70015f10cba7e348b2e42cb4e6
omnitool <biting_duck@hotmail.com>
2015-06-01 01:23:08 -0600

    failing specs which tell me to expand User Model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  it 'saves all attributes' do
+    user = User.new({first_name: 'Philadelphia', last_name: 'Collins', username: 'baaaam', email: 'phil@collins.info', password: 'peanutbutterandjaaaam'})
+    user.save!
+    expect(user).to match({first_name: 'Philadelphia', last_name: 'Collins', username: 'baaaam', email: 'phil@collins.info', password: 'peanutbutterandjaaaam'})
+  end
 end
