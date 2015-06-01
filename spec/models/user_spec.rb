b8071f591be4472ce91ecb8fe916b940c1eabe05
omnitool <biting_duck@hotmail.com>
2015-06-01 03:25:16 -0600

    added spec coverage for username

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,4 +12,13 @@ RSpec.describe User, type: :model do
     expect(user.username).to eq 'baaaam'
     expect(user.email).to eq 'phil@collins.info'
   end
+
+  it 'validates correctly' do
+    user = User.new
+    expect(user).to_not be_valid
+    expect(user.errors[:email]).to include("can't be blank")
+    expect(user.errors[:password]).to include("can't be blank")
+    expect(user.errors[:username]).to include("can't be blank")
+
+  end
 end
